33869a033e980f4a8926ab2285fca0bc9de585bd
Mukund <abhishek.jupyter@gmail.com>
2024-06-01 00:37:29 +0530

    card added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,38 @@ const Main = () => {
     <div className="main">
         <div className="nav">
             <p>Gemini </p>
-            <img src={assets.user_icon} alt="" />
+            <img className='img' src={assets.user_icon} alt="" />
+        </div>
+
+        <div className="main-container">
+          <div className="greet">
+            <p className='name'><span>Hello, Alpha</span></p>
+            <p>How can I help you today?</p>
+          </div>
+
+          <div className="cards">
+            <div className="card">
+              <p>Explain how something works like an engineer</p>
+              <img src={assets.bulb_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Help me pick a movie to watch based on a genre</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Outline an organized & logical sales pitch for a new product</p>
+              <img src={assets.code_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Give me phrases to learn a new language</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+
+            
+          </div>
         </div>
     </div>
   )
