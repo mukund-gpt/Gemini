a573717d6ca47378a00f371311cc5603a9a5fd24
Mukund <abhishek.jupyter@gmail.com>
2024-05-31 23:46:01 +0530

    gemini

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div className="main">
         <div className="nav">
-            <p>Gemini</p>
+            <p>Gemini </p>
             <img src={assets.user_icon} alt="" />
         </div>
     </div>
