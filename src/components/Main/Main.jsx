75d035cf7ce6276383cccc1ffdc6d21c8b90f7ab
Mukund <abhishek.jupyter@gmail.com>
2024-06-01 14:37:09 +0530

    ui complete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -35,8 +35,19 @@ const Main = () => {
               <p>Give me phrases to learn a new language</p>
               <img src={assets.compass_icon} alt="" />
             </div>
+          </div>
 
-            
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder='Enter a prompt here' />
+              <div>
+                <img src={assets.gallery_icon} />
+                <img src={assets.mic_icon} />
+                <img src={assets.send_icon} />
+              </div>
+
+            </div>
+            <p className='bottom-info'>Gemini may display inaccurate info, including about people, so double-check its responses.</p>
           </div>
         </div>
     </div>
