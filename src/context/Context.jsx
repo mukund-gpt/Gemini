126d0c45918278f7258945207d4afba925153bb3
Mukund <abhishek.jupyter@gmail.com>
2024-06-02 21:06:09 +0530

    new-chat complete

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -3,44 +3,54 @@ import run from "../config/gemini";
 
 export const Context = createContext();
 
-const ContextProvider = (props)=>{
+const ContextProvider = (props) => {
 
-    const [input,setInput]=useState("")
-    const [recentPrompt,setRecentPrompt]=useState("")
-    const [prevPrompts,setPrevPrompts]=useState([])
-    const [showResult,setShowResult]=useState(false)
-    const [loading,setLoading]=useState(false)
-    const [resultData,setResultData]=useState(false)
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevPrompts, setPrevPrompts] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState(false)
 
-    const delayPara=(index,nextWord)=>{
-        setTimeout(function(){
-            setResultData(prev=>prev+nextWord)
-        }, 75*index);
+    const delayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord)
+        }, 75 * index);
     }
-    
-    const onSent=async(prompt)=>{
+    const newChat=()=>{
+        setLoading(false)
+        setShowResult(false)
+    }
+    const onSent = async (prompt) => {
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response= await run(input)
-        let responseArray=response.split("**")
-        let newResponse
-        for(let i=0;i<responseArray.length;i++)
-        {
-            if(i===0 || i%2===0)
-            {
-                newResponse+=responseArray[i]
-            }else
-            {
-                newResponse+="<b>"+responseArray[i]+"</b> "
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt) 
+            setRecentPrompt(prompt)
+        } else {
+            setPrevPrompts(prev => [...prev, input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+        // setRecentPrompt(input)
+        // setPrevPrompts(prev=>[...prev,input])
+        // const response= await run(input)
+        let responseArray = response.split("**")
+        let newResponse = ""
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 === 0) {
+                newResponse += responseArray[i]
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b> "
             }
         }
-        let newResponse2=newResponse.split("*").join("</br>")
+        let newResponse2 = newResponse.split("*").join("</br>")
         setResultData(newResponse2)
-        let newResponseArray=newResponse2.split(" ")
-        for(let i=0;i<newResponseArray.length;i++){
-            delayPara(i,newResponseArray[i]+" ")
+        let newResponseArray = newResponse2.split(" ")
+        for (let i = 0; i < newResponseArray.length; i++) {
+            delayPara(i, newResponseArray[i] + " ")
         }
         setLoading(false)
         setInput("")
@@ -50,16 +60,17 @@ const ContextProvider = (props)=>{
     // onSent("What is React js")
 
 
-    const contextValue={
-        input,setInput,
-        recentPrompt,setRecentPrompt,
-        prevPrompts,setPrevPrompts,
+    const contextValue = {
+        input, setInput,
+        recentPrompt, setRecentPrompt,
+        prevPrompts, setPrevPrompts,
         showResult,
         loading,
         resultData,
-        onSent
+        onSent,
+        newChat
     }
-    return(
+    return (
         <Context.Provider value={contextValue}>
             {props.children}
         </Context.Provider>
