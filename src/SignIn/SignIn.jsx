65ba51cc673048b568e87719a6aadc00e7b63d9a
Mukund <abhishek.jupyter@gmail.com>
2024-06-05 14:41:32 +0530

    singin button update

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,21 +1,27 @@
-// src/SignIn.js
-import React from 'react';
+import React, { useState } from 'react'
 import { auth, provider } from './firebaseConfig';
 import { signInWithPopup } from 'firebase/auth';
 
 const SignIn = () => {
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
   const signInWithGoogle = async () => {
+    setIsSigningIn(true);
     try {
       const result = await signInWithPopup(auth, provider);
       console.log(result.user); // User info
     } catch (error) {
       console.error("Error during sign-in:", error);
+    } finally {
+      setIsSigningIn(false);
     }
   };
 
   return (
     <div>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <button onClick={signInWithGoogle} disabled={isSigningIn}>
+        {isSigningIn ? 'Signing in...' : 'Sign in with Google'}
+      </button>
     </div>
   );
 };
